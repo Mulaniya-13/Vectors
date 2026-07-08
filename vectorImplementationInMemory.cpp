e5a90b208e46d4706bf973024f61fa6c202415a7
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-07-08 21:19:41 +0530

    Adding New Files

---
+++ vectorImplementationInMemory.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec= {1,2,3,4};
+    cout<<"Size : "<<vec.size()<<endl; //4
+    cout<<"Capacity : "<<vec.capacity()<<endl; //4
+
+    vec.push_back(5);
+    cout<<"Size : "<<vec.size()<<endl; //5
+    cout<<"Capacity : "<<vec.capacity()<<endl; //8
+
+    vec.pop_back();
+    cout<<"Size : "<<vec.size()<<endl; //4
+    cout<<"Capacity : "<<vec.capacity()<<endl; //capacity remains the same, it doesn't change
+    return 0;
+}
