33ac1ebf6d739c97701ada352d2ec8bded7eb234
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-07-09 22:36:09 +0530

    Adding New Files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,1 @@
- // int size;
-    // cin>>size;
-    
-    // int *arr=new int[size];
-
-    // int x=1;
-    // for(int i=0;i<size;i++){
-    //     arr[i]=x;
-    //     cout<<arr[i]<<" ";
-    //     x++;
-    // }
-    // cout<<endl;
+cout<<ans[0]<<","<<ans[1]<<endl;
